95fe54182381f1e37998b66f67f01218b0fbd27c
lhefdh <lhefdh@gmail.com>
2024-11-17 22:43:10 +0100

    structure et Header

---
+++ src/components/Header.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+import { useLocation } from "react-router-dom";
+import '../style.css';
+
+export default function Header() {
+
+    let location = useLocation();
+
+    const homeLinkIsActive = (location.pathname === "/pages/");
+    const servicesLinkIsActive = (location.pathname === "/pages/Services");
+    const projectsLinkIsActive = (location.pathname === "/pages/Projects");
+    const blogLinkIsActive = (location.pathname === "/pages/Blog");
+    const contactLinkIsActive = (location.pathname === "/pages/Contact");
+
+    return (
+    <nav className="navbar navbar-expand-lg bg-dark text-uppercase">
+        <div className="container-fluid ms-5" >
+            <a className="navbar-brand text-white" href="#">john doe</a>
+            <div className="d-flex justify-content-end me-5" id="navbarSupportedContent">
+                <ul className="navbar-nav mb-2 mb-lg-0 text-secondary">
+                    <li className="nav-item">
+                        <Link className={homeLinkIsActive? "nav-link active":"nav-link text-secondary"} to="../pages/">accueil</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className={servicesLinkIsActive? "nav-link active":"nav-link text-secondary"} to="../pages/Services">services</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className={projectsLinkIsActive? "nav-link active":"nav-link text-secondary"} to="../pages/Projects">mes réalisations</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className={blogLinkIsActive? "nav-link active":"nav-link text-secondary"} to="../pages/Blog">mon blog</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className={contactLinkIsActive? "nav-link active":"nav-link text-secondary"} to="../pages/Contact">contact</Link>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        
+    </nav>
+    );
+}
---
+++ src/pages/NotFound.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function NotFound() {
+    return (
+      <div>
+        <h1>Test NotFound</h1>
+      </div>
+    );
+}
